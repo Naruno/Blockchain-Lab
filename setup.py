97fe79c3df5b2934782f4508843716166423a247
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-01-09 21:22:38 +0000

    Changed the version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='blockchain_lab',
-version='0.1.0',
+version='0.2.0',
 description="""A fully functional blockchain lab.""",
 long_description="""
 # Blockchain Lab
